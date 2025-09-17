1ca0649ea8fb1338fe7daca72675bb30e5126c43
AbdooOwd <benari102@gmail.com>
2025-09-18 00:31:50 +0100

    speaker position relative to screen height

--- src/gameloop.rs
+++ src/gameloop.rs
@@ -10,5 +10,5 @@ pub async fn update_speaker_emotion_texture(emotion: SpeakerEmotion, speaker_tex
     let emotion_texture_path = get_emotion_texture_path(emotion);
     let new_image = load_image(emotion_texture_path.as_str()).await.unwrap();
     speaker_tex.update(&new_image);
-    draw_from_texture(speaker_tex, 50.0, 60.0, SPEAKER_TEXTURE_SCALER);
+    draw_from_texture(speaker_tex, 50.0, screen_height() - f32::from(new_image.height) * SPEAKER_TEXTURE_SCALER, SPEAKER_TEXTURE_SCALER);
 }
